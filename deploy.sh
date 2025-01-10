f5df5fd4aa1bd6aa991eef36777138e3760eb1f1
NadeenMK <nadeenmkhalfe@gmail.com>
2025-01-10 20:52:41 +0200

    deply.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-# Pull the latest code
-git pull origin main
-
 # Stop and remove running containers
-docker-compose down
+sudo docker compose down
 
 # Build and run containers
-docker-compose up -d --build
+sudo docker compose up -d --build
